450aba31d9ea5a5396b4c33e3609b4a626768f71
tikis23 <edvis01x@gmail.com>
2021-09-25 21:07:36 +0300

    Update main.cpp

--- graph drawer/src/main.cpp
+++ graph drawer/src/main.cpp
@@ -1,24 +1,6 @@
 #include "GraphDrawer.h"
-#include <iostream>
-#include <string>
-#include "e_eval/e_eval.h"
-#include <vector>
 int main()
 {
-	//std::vector<e_token> tokens;
-	//std::vector<e_operand> operands;
-	//std::string function;
-	//std::cin >> function;
-	//int error;
-	//tokens = e_compile(function, error);
-	//std::cout << e_get_error_message(error) << "\n";
-	//operands = e_get_operands(tokens);
-	//double ats = e_evaluate(tokens, operands, error);
-	//std::cout << e_get_error_message(error) << "\n";
-
-	//std::cout << "\n" << ats << "\n";
-	//return 0;
-
 	GraphDrawer program;
 	return 0;
 }
